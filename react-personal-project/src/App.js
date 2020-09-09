a63dac7f49b19f296a0616a7f5e46b201ef5b990
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-09 10:17:25 +0100

    jsx restrictions

--- react-personal-project/src/App.js
+++ react-personal-project/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hi , I'm a React App</h1>
-  //   </div>
-  // );
-  return React.createElement('div', { className: 'App' },
-    React.createElement('h1', null, 'Hi I\'m a React App!!!'))
+  return (
+    <div className="App">
+      <h1>Hi , I'm a React App</h1>
+      <p>This is really working</p>
+    </div>
+  );
+  // return React.createElement('div', { className: 'App' },
+  //   React.createElement('h1', null, 'Hi I\'m a React App!!!'))
 }
 
 export default App;
