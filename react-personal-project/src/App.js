a1a6affe0e95ff397ff3c9b7ba2fb9d094c49bd9
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-09 10:12:41 +0100

    understanding jsx

--- react-personal-project/src/App.js
+++ react-personal-project/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi , I'm a React App</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hi , I'm a React App</h1>
+  //   </div>
+  // );
+  return React.createElement('div', { className: 'App' },
+    React.createElement('h1', null, 'Hi I\'m a React App!!!'))
 }
 
 export default App;
