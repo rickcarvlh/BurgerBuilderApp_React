fd052eec45949806475807ee0d74c70ac91aacb8
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-09-16 16:17:00 +0100

    changes to the app.js

--- react-personal-project/src/App.js
+++ react-personal-project/src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi , I'm a React App</h1>
-      <p>This is really working</p>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29">My Hobbies: Racing</Person>
-      <Person name="Stephanie" age="30" />
-    </div>
-  );
-  // return React.createElement('div', { className: 'App' },
-  //   React.createElement('h1', null, 'Hi I\'m a React App!!!'))
+class App extends Component{
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Hi , I'm a React App</h1>
+        <p>This is really working</p>
+        <button>Switch Name</button>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="30" />
+      </div>
+    );
+    // return React.createElement('div', { className: 'App' },
+    //   React.createElement('h1', null, 'Hi I\'m a React App!!!'))
+  }
 }
 
 export default App;
