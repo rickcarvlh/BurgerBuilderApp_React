fa79faf2a505cb0461119b19715ceb268258ac76
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-10-04 14:50:28 +0100

    error boundaries

--- react-personal-project/src/App.js
+++ react-personal-project/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
-
 import Person from './Person/Person';
-
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 const StyledButton = styled.button`
     background-color: ${props => props.myAlt ? 'red' : 'green'};
@@ -75,13 +74,15 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangeHandler(event, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+
+                  changed={(event) => this.nameChangeHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             )
           })}
         </div>
