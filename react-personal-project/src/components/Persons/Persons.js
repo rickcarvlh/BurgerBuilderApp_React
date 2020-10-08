dd8ec94c0f0b0eaa253ab99aafd1c2d8d552748a
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-10-08 15:09:34 +0100

    usinh shouldComponentUpdate - optimization

--- react-personal-project/src/components/Persons/Persons.js
+++ react-personal-project/src/components/Persons/Persons.js
@@ -15,7 +15,12 @@ class Persons extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         // isto não é assim que se faz
         console.log('[Persons.js], shouldComponentUpdate..');
-        return true;
+        if (nextProps.persons !== this.props.persons) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     getSnapshotBeforeUpdate(prevProps, prevState) {
