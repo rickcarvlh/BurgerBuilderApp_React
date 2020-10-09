347692eeffbcd53e91f09ca6c3295b5b42bd6ad8
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-10-09 15:10:37 +0100

    rendering adjacent jsx elements

--- react-personal-project/src/components/Persons/Person/Person.js
+++ react-personal-project/src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import Auxiliary from '../../../hoc/Auxiliary'
 // import styled from 'styled-components';
-import classes from './Person.module.css'
+// import classes from './Person.module.css'
 // import './Person.css';
 
 /*
@@ -23,18 +24,19 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>
+            <Auxiliary>
+                <p key="i1" onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
         </p>
-                <p>{this.props.children}</p>
+                <p key="i2">{this.props.children}</p>
                 <input
+                    key="i3"
                     type="text"
                     onChange={this.props.changed}
                     value={this.props.name}
                 />
-            </div>
-        );
+            </Auxiliary>
+        )
     }
 }
 
---
+++ react-personal-project/src/hoc/Auxiliary.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Auxiliary = props => props.children;
+
+
+export default Auxiliary;
